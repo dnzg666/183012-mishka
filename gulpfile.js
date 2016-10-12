ff3be10fd5776c668c2f85aeb205493b42d11912
Daniel Chikimoni <dnzg666@gmail.com>
2016-10-12 03:11:01 +0200

    сделал автоматизацию

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,18 @@ var postcss = require("gulp-postcss");
 var precss = require("precss");
 var postcssInlineSvg = require("postcss-inline-svg");
 var autoprefixer = require("autoprefixer");
+
+var mqpacker = require("css-mqpacker");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+
+var imagemin = require("gulp-imagemin");
+
 var server = require("browser-sync").create();
+
+var run = require("run-sequence");
+
+var del = require("del");
 
 gulp.task("style", function() {
   gulp.src("postcss/style.css")
@@ -20,15 +31,30 @@ gulp.task("style", function() {
         "last 2 Firefox versions",
         "last 2 Opera versions",
         "last 2 Edge versions"
-      ]})
+      ]}),
+      mqpacker({
+        sort: true
+      })
     ]))
-    .pipe(gulp.dest("css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("images", function() {
+  return gulp.src("build/img/**/*.{png,jpg,gif}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true})
+    ]))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("serve", function() {
   server.init({
-    server: ".",
+    server: "build",
     notify: false,
     open: true,
     ui: false
@@ -36,4 +62,30 @@ gulp.task("serve", ["style"], function() {
 
   gulp.watch("postcss/**/*.css", ["style"]);
   gulp.watch("*.html").on("change", server.reload);
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "fonts/**/*.{woff,woff2}",
+    "img/**",
+    "js/**",
+    "*.html"
+  ], {
+    base: "."
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("build", function(fn) {
+  run(
+    "clean",
+    "copy",
+    "style",
+    "images",
+    fn
+  );
 });
